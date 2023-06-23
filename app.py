b98b5a962e668908049ed0d3b7a5a996431404f0
KianSabzi <sabzi.morteza@gmail.com>
2023-06-23 18:09:07 +0330

    improve flask api configuration and read from external resource.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
-import os
+from datetime import timedelta
+import os,json
 
 from db import db
 import models
@@ -10,23 +11,17 @@ from resources.task import blp as ItemBlueprint
 from resources.category import blp as StoreBlueprint
 from resources.user import blp as UserBlueprint
 
+Access_Expires = timedelta(hours=1)
+
 def create_app(db_url=None):
     app = Flask(__name__)
 
-    app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["API_TITLE"] = "FlaskAPI"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config.from_file("settings.json", load=json.load)
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = Access_Expires
+
     db.init_app(app)
-
     api = Api(app)
-
-    app.config["JWT_SECRET_KEY"] = "118807697803134057398618144851912555542"
     jwt = JWTManager(app)
 
     with app.app_context():
