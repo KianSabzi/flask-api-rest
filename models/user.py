b517a2b05d3ebcad1b6ad6e059bf6c1036077b79
KianSabzi <sabzi.morteza@gmail.com>
2023-06-26 01:11:49 +0330

    Change lenght of password property in user model.

--- models/user.py
+++ models/user.py
@@ -6,6 +6,6 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_identifier = db.Column(db.String(80), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     tasks = db.relationship("TaskModel", back_populates="user", lazy="dynamic" , cascade="all, delete")
     categories = db.relationship("CategoryModel", back_populates="user", lazy="dynamic" , cascade="all, delete")
