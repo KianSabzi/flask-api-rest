2bf5b37f4c9a6d9fe22b16a3f6062b9ed11a4729
KianSabzi <sabzi.morteza@gmail.com>
2023-07-02 00:48:55 +0330

    Modify user model and add migration for it.

--- models/user.py
+++ models/user.py
@@ -5,7 +5,9 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_identifier = db.Column(db.String(80), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(250))
+    email = db.Column(db.String(80), unique=True)
+    phone_number = db.Column(db.String(30), unique=True, nullable=False)
     tasks = db.relationship("TaskModel", back_populates="user", lazy="dynamic" , cascade="all, delete")
     categories = db.relationship("CategoryModel", back_populates="user", lazy="dynamic" , cascade="all, delete")
